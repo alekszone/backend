7fbb97ebcff8362e1a107f10efb1a03b519d6450
segundara <segundara@gmail.com>
2020-07-22 16:35:32 +0300

    img

--- src/routes/profiles/index.js
+++ src/routes/profiles/index.js
@@ -227,7 +227,7 @@ profileRouter.post('/:username/picture', upload.single('userImage'), async (req,
     await fs.writeFile(path.join(imagePath, `${req.params.username}.jpg`), req.file.buffer)
 
     req.body = {
-      image: `https://linkedin-team.herokuapp.com/image/profile/${req.params.username}.jpg`
+      image: `https://linkedin-team.herokuapp.com/image/profile/${req.params.username}`
     }
     const user = await ProfileModel.findOneAndUpdate({ 'username': req.params.username }, req.body)
     if (user) {
