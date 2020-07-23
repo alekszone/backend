a8b0be5757151d3cdc46026c34c3da0ef199b869
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-07-23 16:26:13 +0200

    new updateeee

--- src/routes/posts/postSchema.js
+++ src/routes/posts/postSchema.js
@@ -1,28 +1,27 @@
-const {Schema} =  require("mongoose")
+const { Schema } = require("mongoose")
 const mongoose = require("mongoose")
 
 
 const post = new Schema({
-text:{
-    type:String,
-    required:true
+    text: {
+        type: String
+    },
+    username: {
+        type: String
+
+    },
+    user: [{
+        type: Schema.Types.ObjectId, ref: "profiles"
+    }],
+    image: {
+        type: String,
+    },
 },
-username : {
-    type:String,
-    required:true
-},
-user:[{
-  type:Schema.Types.ObjectId , ref:"profiles"
-}],
-image:{
-    type:String,    
-},
- },
-{
-timestamps:true
-})
-post.static("addProfile", async function(id){
-    const profile = await PostsModel.findOne({_id:id}).populate("user")
+    {
+        timestamps: true
+    })
+post.static("addProfile", async function (id) {
+    const profile = await PostsModel.findOne({ _id: id }).populate("user")
     return profile
 })
 
